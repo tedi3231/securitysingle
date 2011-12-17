fe51920b1f7a0d1ebad95d84a03d6957ffe9403d
tedi3231 <tedi3231@gmail.com>
2011-12-17 12:09:06 +0800

    modified the database password

--- blog.py
+++ blog.py
@@ -36,7 +36,7 @@ define("port", default=8888, help="run on the given port", type=int)
 define("mysql_host", default="127.0.0.1:3306", help="blog database host")
 define("mysql_database", default="blog", help="blog database name")
 define("mysql_user", default="root", help="blog database user")
-define("mysql_password", default="loveyuanyuan", help="blog database password")
+define("mysql_password", default="password", help="blog database password")
 
 
 class Application(tornado.web.Application):
